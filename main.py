196c7e1c9dc3fd0606308482b8bafd4aeddfb80b
Joe Hu <Joe at Home>
2023-10-03 11:19:07 +0800

    目前卡關A矩陣建立

---
+++ main.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+from numpy import empty
+# Load rainfall data from a JSON file
+with open('rainfall_data.json', 'r') as json_file:
+    data = json.load(json_file)    
+    rainfall = data['rainfall']
+    runoff = data['runoff']
+    Time = empty(len(rainfall))
+
+#define the lengh of unithydrograph
+for i in range(len(rainfall)-1 , -1, -1):
+    if rainfall[i] != 0:
+        last_non_zero = rainfall[i]        
+        break
+#unit_hydrograph = empty(len(runoff)-i)
+
+smallrainfall = rainfall[:i+1]
+# Create a unit hydrograph by dividing the runoff by the peak runoff value
+#Amatrix 
+
+aMatreix = np.empty((len(rainfall),len(runoff)-i+1))
+
+print(len(rainfall))
+print(len(runoff)-i+1)
+
+for i in range (0,len(rainfall)-1,1):
+    for j in range (0,len(smallrainfall),1):
+        aMatreix[i,i+j]=smallrainfall[j]t
+        
+
+
+
+#unit_hydrograph = 
+
+
+
+#print(len(runoff)-i)
+
+
+
+# Plot the unit hydrograph
+'''
+plt.plot(unit_hydrograph)
+plt.xlabel('Time (hours)')
+plt.ylabel('Unit Hydrograph')
+plt.title('Unit Hydrograph')
+plt.grid(True)
+plt.show()
+'''
